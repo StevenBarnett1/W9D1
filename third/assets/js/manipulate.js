8da899438fedc731c35f99403d39f916eb2a6268
Stevenbarnett1 <Stevenbarnett1@live.com>
2021-07-26 16:51:21 -0700

    finished third

---
+++ third/assets/js/manipulate.js
@@ -0,0 +1,18 @@
+export function changeTitle() {
+    // Change the title of the page to "(Your name)'s Portfolio"
+    document.title = "Joes Portfolio"
+    // Your code here
+}
+
+export function changeHeader() {
+    // Change the name in the h1 of the page to your name
+    document.body.children[0].children[0].innerText = "I am Joe"
+    // Your code here
+}
+
+export function changeAboutMe() {
+    /* Update the first paragraph in the About Me section with a small
+     passage about yourself */
+    document.body.children[1].children[1].innerText = "Something about Joe"
+    // Your code here
+}
---
+++ third/assets/js/search.js
@@ -0,0 +1,30 @@
+export function findElementById(id) {
+    // Return the element in the DOM with corresponding `id`
+    return document.getElementById(id)
+    // Your code here
+}
+
+export function findFirstElementOfTag(tag) {
+    // Return the first occurence of an element of tag name `tag`
+    return document.getElementbyTagName(tag)[0]
+    // Your code here
+}
+
+export function findFirstElementOfClass(cls) {
+    // Return the first occurence of an element of class `cls`
+    return document.getElementsByClassName(cls)[0]
+    // Your code here
+}
+
+export function findElementsOfTag(tag) {
+
+    // Return an array of elements that have a tag name of `tag`
+    return document.getElementbyTagName(tag)
+    // Your code here
+}
+
+export function findElementsOfClass(cls) {
+    // Return an array of elements that have are of class `cls`
+    return document.getElementsByClassName(cls)
+    // Your code here
+}
